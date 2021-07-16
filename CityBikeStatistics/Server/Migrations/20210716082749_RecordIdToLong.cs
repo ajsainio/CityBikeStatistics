132643ee08e2f8adb7798bd2afbc1bae56ae39ed
Antti Sainio <antti.j.sainio@innofactor.com>
2021-07-16 13:40:43 +0300

    Bike record id migration

---
+++ CityBikeStatistics/Server/Migrations/20210716082749_RecordIdToLong.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CityBikeStatistics.Server.Migrations
+{
+    public partial class RecordIdToLong : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "RecordId",
+                table: "CityBikeData",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "RecordId",
+                table: "CityBikeData",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
