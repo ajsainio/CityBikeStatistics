a5520812bc07f3967ad659a70e7a5aa24c90546e
Antti Sainio <antti.j.sainio@innofactor.com>
2021-07-16 13:37:33 +0300

    Use long for record id

--- CityBikeStatistics/Server/Data/CityBikeDataMap.cs
+++ CityBikeStatistics/Server/Data/CityBikeDataMap.cs
@@ -1,10 +1,11 @@
-﻿using CityBikeStatistics.Shared;
+﻿using System;
+using CityBikeStatistics.Shared;
 using CsvHelper.Configuration;
 
 namespace CityBikeStatistics.Server.Data {
   public sealed class CityBikeDataMap : ClassMap<CityBikeDataContract> {
     public CityBikeDataMap(int year, int month) {
-      Map(m => m.RecordId).Convert(record => int.Parse("" + year + month + record.Row.Parser.Row));
+      Map(m => m.RecordId).Convert(record => long.Parse("" + year + month + record.Row.Parser.Row));
       Map(m => m.Departure);
       Map(m => m.Return);
       Map(m => m.DepartureStationId).Name("Departure station id");
